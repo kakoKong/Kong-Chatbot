9353e8183ac6bda3ecd2591d59d32fe742549d5c
Kong Nopwattanapong <kong.nopwattanapong@credera.co.uk>
2023-12-04 13:23:00 +0000

    add more conditions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
-import pyaudio
 import speech_recognition as sr
+from gtts import gTTS
+import os
+import datetime
 
 class ChatBot():
   def __init__(self, name):
@@ -12,6 +14,7 @@ class ChatBot():
     with sr.Microphone() as mic:
       print("Listening...")
       audio = recognizer.listen(mic)
+
     try:
       self.text = recognizer.recognize_google(audio)
       print("me --> ", self.text)
@@ -20,7 +23,17 @@ class ChatBot():
       print("me --> ERROR", str(e))
 
   def wake_up(self, text):
-    return True if self.name in text.lower() else False
+    return True if "hello" in text.lower() else False
+  
+  def text_to_speech(self, text):
+    print('AI --> ', text)
+    speaker = gTTS(text=text, lang="en", slow=False)
+    speaker.save("res.mp3")
+    os.system("afplay res.mp3")
+    os.remove("res.mp3")
+
+  def action_time(self):
+    return datetime.datetime.now().time().strftime('%H:%M')
 
 if __name__ == "__main__":
   ai = ChatBot(name="Dev")
@@ -28,3 +41,12 @@ if __name__ == "__main__":
     ai.speech_to_text()
     if (ai.wake_up(ai.text)):
       res = "Hello I am Dev in AI, what can I do for you?"
+
+    elif "time" in ai.text:
+      res = ai.action_time()
+
+    elif (any(i in ai.text for i in ["thanks", "thank"])):
+      res = np.random.choice(["you're welcome!", "anytime", "no problem", "chill", "Nice to meet you!"])
+    
+    print(res)
+    ai.text_to_speech(res)
